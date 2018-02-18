15ada280a8a214515857098a25ab92803c6f7003
Monika <m.zebuolyte@gmail.com>
2018-02-18 20:08:22 +0200

    change title

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
     return (
       <div className="header">
             <h1> 
-                Parduotuvė
+                Monikos bilietai
             </h1>
             <div className='nav'>
               <ul>
